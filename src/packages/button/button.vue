7aa667dfc5a43e436d03e2f4c2ea29eb97cd186a
lizy6 <lizy6@asiainfo.com>
2019-10-28 14:35:59 +0800

    test02

---
+++ src/packages/button/button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    class="lime-button"
+    :class="{ [`lime-button-${type}`]: true }"
+    type="button"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+import { oneOf } from "../../utils/assist";
+
+export default {
+  name: "Button",
+  props: {
+    type: {
+      validator(value) {
+        return oneOf(value, [
+          "default",
+          "primary",
+          "info",
+          "success",
+          "warning",
+          "error",
+          "danger"
+        ]);
+      },
+      type: String,
+      default: "default"
+    }
+  }
+};
+</script>
